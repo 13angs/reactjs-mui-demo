98fe62123a27d451c908473dd1c44ca0a019d852
Kimlupusee <KimmyLps.Dev@gmail.com>
2022-07-28 04:43:14 +0000

    init pdf export component

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ProductList from './pages/product/list.product';
 import SvgImages from './pages/image/svg.image';
+import ExportToPDF from './pages/pdf/ExportToPDF';
 import routes from './routes/routes';
 
 export default function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path={routes.image.svg} component={SvgImages} />
-          <Route path={routes.product} component={ProductList} />
-        </Switch>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path={routes.image.svg} component={SvgImages} />
+            <Route path={routes.product} component={ProductList} />
+            <Route path={routes.pdf} component={ExportToPDF} />
+          </Switch>
+        </BrowserRouter>
     </div>
   );
 }
